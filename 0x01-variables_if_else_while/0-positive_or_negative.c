662920a797c1ea3d00cb06ff037f8b87478549fb
Bigdykky <dikechined@gmail.com>
2022-12-11 11:04:45 -0800

    mission is possible

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/*More headers goes there*/
+
+/*betty style doc function main goes there*/
 /**
- * main - entry point
- * Return: 0
+ * main -main function generates a random number
+ *
+ * Return: Always 0 (success)
 */
 int main(void)
 
@@ -11,13 +16,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	/* your code goes there */
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
+	}
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 
 }
