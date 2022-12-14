22d1de37d97a433b8d6ec55187c81226938ac800
Bigdykky <dikechined@gmail.com>
2022-12-13 21:02:52 -0800

    it worked

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - i am here today
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 133)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
