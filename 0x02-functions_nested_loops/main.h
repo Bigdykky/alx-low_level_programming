8e267044ce145045276b2fd10a988ff2300af22e
Bigdykky <dikechined@gmail.com>
2022-12-17 01:15:12 -0800

    information

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,9 +7,7 @@
  *
  * Author:    Dike Chinedu
  *
- * Desc: Header file containing declarations for all functions
- *
- *       used in the 0x02-functions_nested_loops directory.
+ * Desc: Header file containing declarations for all functions used in the 0x02-functions_nested_loops directory.
  *
 */
 
