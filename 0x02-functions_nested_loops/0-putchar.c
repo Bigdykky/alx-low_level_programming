63e31ca05bcf6f1ae85b49618c374c58517a5d92
Bigdykky <dikechined@gmail.com>
2022-12-17 01:07:18 -0800

    it must work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,24 @@
-#include <main.h>
+#include "main.h"
 /**
- * main - Entry point
- * Return: 0
+ * main - Prints “_putchar” followed by a new line.
+ *
+ * Return: Always 0.
+ *
 */
 
 int main(void)
 
 {
+	_putchar(‘_’);
+	_putchar(‘p’);
+	_putchar(‘u’);
+	_putchar(‘t’);
+	_putchar(‘c’);
+	_putchar(‘h’);
+	_putchar(‘a’);
+	_putchar(‘r’);
+	_putchar(‘\n’);
 
-		char *sh = "_putchar";
+	return (0);
 
-		while (*sh)
-		{
-			putchar(*sh);
-
-			sh++;
-		}
-		putchar('\n');
-
-		return (0);
 }
