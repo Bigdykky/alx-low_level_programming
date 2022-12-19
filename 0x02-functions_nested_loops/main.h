7184b98bde54a95af0a4f59aa197c5292d75b9ec
Bigdykky <dikechined@gmail.com>
2022-12-19 07:31:07 -0800

    remapped

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- *
- * File: main.h
- *
- * Author:    Dike Chinedu
- *
- * Desc: Header file containing declarations for all functions used in the
- *
-*/
+#include <stdio.h>
 
 int _putchar(char c);
 void print_alphabet(void);
