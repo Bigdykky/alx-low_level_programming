69d605d9ddcb7101f652eea9dd5da556757a1705
Bigdykky <dikechined@gmail.com>
2022-12-21 01:14:28 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - causes an infinite loop
- * Return: 0
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
 */
 
 int main(void)
 
 {
-	int i;
+	print_alphabet();
 
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	/*while (i < 10)*/
-	/*{*/
-	/* putchar(i);*/
-	/*}*/
-	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
