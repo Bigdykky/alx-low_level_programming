15006d6bbce2e81ba009ba030b3f29eddd25fd31
Bigdykky <dikechined@gmail.com>
2022-12-22 11:17:03 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void _puts(char *str);
 int _strlen(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -8,4 +9,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void _puts(char *str);
+
