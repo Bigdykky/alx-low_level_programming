4df31805fa296b69639dad938e505380dba2038b
Bigdykky <dikechined@gmail.com>
2022-12-27 08:50:55 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,113 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+/**
+ * _putchar - prints a character
+ * @c: char to be printed
+ * Return: 0
+ */
+
+
+int _putchar(char c);
+
+
+/**
+ * reset_to_98 - changes a var value to 98
+ * @n: a pointer to the address of the variable
+ * Return: void
+ */
+
+
 void reset_to_98(int *n);
+
+
+/**
+ * swap_int - swaps the values of 2 variables
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+
+
 void swap_int(int *a, int *b);
+
+
+/**
+ * _strlen - checks the length of a string
+ * @s: the string checked
+ * Return: result
+ */
+
+
 int _strlen(char *s);
+
+
+/**
+ * _puts - prints strings
+ * @str: string to print
+ * Return: void
+ */
+
+
 void _puts(char *str);
+
+
+/**
+ * print_rev - prints strings in reverse
+ * @s: string to print
+ * Return: void
+ */
+
+
 void print_rev(char *s);
+
+
+/**
+ * rev_string - reverse strings
+ * @s: string to rev
+ * Return: void
+ */
+
+
 void rev_string(char *s);
+
+
+/**
+ * puts2 - prints every other char 
+ * *str: string to print
+ * Return: void
+ */
+
 void puts2(char *str);
+
+/**
+ * puts_half - prints 2nd half of a string
+ * @str: string
+ * Return: void
+ */
+
 void puts_half(char *str);
+
+/**
+ * print_array - prints elements of an array
+ * @a: thr array
+ * @n: number of elements
+ * Return: void
+ */
+
 void print_array(int *a, int n);
+
+
+/**
+ * _strcpy - copies string between arrays
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
+
+#endif
