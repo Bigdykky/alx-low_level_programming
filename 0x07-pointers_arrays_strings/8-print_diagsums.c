2fd7ce19e8a5bfc28d7d56ae69e4ca451f0791bc
Bigdykky <dikechined@gmail.com>
2023-01-04 15:14:01 -0800

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sums of the diagonal of a matrice
+ * @a: array of matrice
+ * @size: size of the Matrice
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int row, i, s1, s2;
+
+	s1 = 0;
+	s1 = 0;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		s1 += a[i];
+	}
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		s2 += a[i];
+	}
+	printf("%d, %d\n", s1, s2);
+}
